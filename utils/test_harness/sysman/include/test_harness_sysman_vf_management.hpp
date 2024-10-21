a3311e18efb7f1978bd5c8726861a1bf23cd366b
Vishnu Khanth <vishnu.khanth.b@intel.com>
2024-10-21 15:55:54 +0530

    test: Add CTS for VFManagementGetVFCapabilities
    Related-To: VLCLJ-2315
    
    Signed-off-by: Vishnu Khanth <vishnu.khanth.b@intel.com>

--- utils/test_harness/sysman/include/test_harness_sysman_vf_management.hpp
+++ utils/test_harness/sysman/include/test_harness_sysman_vf_management.hpp
@@ -18,7 +18,11 @@ std::vector<zes_vf_handle_t> get_vf_handles(zes_device_handle_t device,
                                             uint32_t &count);
 zes_vf_exp_capabilities_t get_vf_capabilities(zes_vf_handle_t vf_handle);
 uint32_t get_vf_mem_util_count(zes_vf_handle_t vf_handle);
-std::vector<zes_vf_util_mem_exp2_t> get_vf_mem_util(zes_vf_handle_t vf_handle, uint32_t &count);
+std::vector<zes_vf_util_mem_exp2_t> get_vf_mem_util(zes_vf_handle_t vf_handle,
+                                                    uint32_t &count);
+uint32_t get_vf_engine_util_count(zes_vf_handle_t vf_handle);
+std::vector<zes_vf_util_engine_exp2_t>
+get_vf_engine_util(zes_vf_handle_t vf_handle, uint32_t &count);
 
 } // namespace level_zero_tests
 
